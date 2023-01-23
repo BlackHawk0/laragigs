e9c8e2293d4e7806d654f9bd685835a529dda89d
Daniel <danielngoys@outlook.com>
2023-01-24 00:43:58 +0300

    route taking an id/integer

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,8 @@ Route::get('/hello', function() {
     return response('<h1>Hello world</h1>');
 });
 
-Route::get('/posts/{id}', function($id){
-    return response('post'. $id);
-}) -> where('id', '[0-9]+');
+Route::get('/posts/{id}', function ($id) {
+    return response('post' . $id);
+})->where('id', '[0-9]+');
 
 
-Route::get('/search', function(Request $request){
-    return $request ->name;
-});
