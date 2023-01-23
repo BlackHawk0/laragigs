1ca8f4cd91232aba20e2998b6568a5867933f21c
Daniel <danielngoys@outlook.com>
2023-01-24 00:48:50 +0300

    render json file in the api

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// render json file
+//  GET api/posts
+Route::get('/posts', function() {
+    return response() -> json([
+        'posts' =>[
+            [
+                'title' => 'Post one'
+            ]
+        ]
+    ]);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
