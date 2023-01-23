ca73ec1420ab644ac45924e7aba00bd1523a3e9d
Daniel <danielngoys@outlook.com>
2023-01-24 00:44:25 +0300

    route accepting several string params

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,6 @@ Route::get('/posts/{id}', function ($id) {
     return response('post' . $id);
 })->where('id', '[0-9]+');
 
-
+Route::get('/search', function (Request $request) {
+    return $request->name;
+});
